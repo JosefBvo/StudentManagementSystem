bd7b0b92713b106567bc0ecd4fe018e765e7826e
JosefBvo <154392935+JosefBvo@users.noreply.github.com>
2024-11-28 16:19:07 -0600

    Update Subject.java

--- Subject.java
+++ Subject.java
@@ -1,4 +1,8 @@
-package com.mycompany.StudentManagementSystem;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.studentmanagementsystem_1;
 
 /**
  *
@@ -20,4 +24,5 @@ public class Subject {
         return SubjectName;
     }
 }
+
 
